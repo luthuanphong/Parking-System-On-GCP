65c2e47a6847007ee89ee8b6a0b0881b234888eb
PhongLu <luthuanphong@gmail.com>
2025-10-15 23:28:41 +0700

    Upload docker files

--- Infras/main.tf
+++ Infras/main.tf
@@ -295,3 +295,10 @@ private_visibility_config {
     }
   }
 }
+
+resource "google_artifact_registry_repository" "parking_api_repo" {
+  location      = var.region
+  repository_id = "parking-api-repo"
+  format        = "DOCKER"
+  description   = "Parking API repository for container images"
+}
